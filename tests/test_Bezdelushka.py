b94d6298701aac156a919d107206259d6e3f2cac
Vladislav <tesei87arieds@gmail.com>
2025-01-14 11:38:44 +0300

    Merge remote-tracking branch 'origin/main'

--- tests/test_Bezdelushka.py
+++ tests/test_Bezdelushka.py
@@ -4,8 +4,8 @@ import Bezdelushka
 
 class QwertyTest(TestCase):
     def test_calculate_functions(self):
-        self.assertEqual(Bezdelushka)
+        self.assertEqual(Bezdelushka.MathematicalClass(1,1),1)
 
 
-if __name__ == '__main__':
-    main()
+        if __name__ == '__main__':
+            main()
